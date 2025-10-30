f731d073a79d3b47eb7712207bd4815660b6c23e
Martin Baron <mato.baron12@gmail.com>
2025-10-30 21:14:10 +0100

    main py

--- chunker/main.py
+++ chunker/main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, json, request, jsonify
-from chunking_evaluation.chunking import RecursiveTokenChunker
+from chunking_evaluation.chunking import RecursiveTokenChunker, KamradtModifiedChunker
 from analyze_chunks import analyze_chunks
-
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 app = Flask(__name__)
 
 @app.route('/rtc', methods=['POST'])
@@ -34,11 +36,42 @@ def rtc():
             "content": recursive_character_chunks,
         }, f, ensure_ascii=False, indent=2)
 
-
-
-    # print(analysis_results)
-    # print(recursive_character_chunks)
     return jsonify(recursive_character_chunks)
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+def get_embedding(text, model="text-embedding-3-small"):
+    if isinstance(text, list):  # batch mode
+        texts = [t.replace("\n", " ") for t in text]
+        response = client.embeddings.create(input=texts, model=model)
+        return [res.embedding for res in response.data]  # list of embeddings
+    else:  # single string
+        cleaned = text.replace("\n", " ")
+        response = client.embeddings.create(input=[cleaned], model=model)
+        return response.data[0].embedding
+
+@app.route('/semantic', methods=['POST'])
+def semantic():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+
+    document = file.read().decode('utf-8')
+
+    kamradt_chunker = KamradtModifiedChunker(
+        avg_chunk_size=600,
+        min_chunk_size=400,
+        embedding_function=get_embedding
+    )
+
+    kamradt_chunks = kamradt_chunker.split_text(document)
+    analyze_chunks(kamradt_chunks, use_tokens=True)
+
+    return jsonify(kamradt_chunks)
 
 if __name__ == '__main__':
     app.run(debug=True)
