eec4b212e2507e1e278fd1e68125f16a0b54cd05
Martin Baron <mato.baron12@gmail.com>
2025-10-30 21:13:04 +0100

    ts schemas

---
+++ app/src/typesense/schemas/typesenseSchema.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const typesenseSchema = {
+	name: 'typesenseSchema',
+	fields: [
+		{ name: 'block_id', type: 'string' },
+		{ name: 'content', type: 'string' },
+		{
+			name: 'vector',
+			type: 'float[]',
+			embed: {
+				from: ['content'],
+				model_config: {
+					model_name: 'openai/text-embedding-3-small',
+					api_key: process.env.OPENAI_API_KEY || ''
+				}
+			},
+			optional: true
+		}
+	]
+};
+
+export default typesenseSchema;
